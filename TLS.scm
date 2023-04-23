ef5c03fd3a0c1885101ea6b815b2cfdff69c1571
MDBoswinkel <mdboswinkel@outlook.com>
2023-04-23 13:49:14 +0200

    Code for sixth chapter added

--- TLS.scm
+++ TLS.scm
@@ -295,3 +295,47 @@
       ((null? l) '())
       ((equal? (car l) s) (cdr l))
       (else (cons (car l) (rember s (cdr l)))))))
+
+(define numbered?
+  (lambda (aexp)
+    (cond
+      ((atom? aexp) (number? aexp))
+      (else (and (numbered? (car aexp)) (numbered? (car (cdr (cdr aexp)))))))))
+
+(define value
+  (lambda (nexp)
+    (cond
+      ((atom? nexp) nexp)
+      ((eq? (operator nexp) '+) (o+ (value (1st-sub-exp nexp)) (value (2nd-sub-exp nexp))))
+      ((eq? (operator nexp) 'x) (* (value (1st-sub-exp nexp)) (value (2nd-sub-exp nexp))))
+      (else (expt (value (1st-sub-exp nexp)) (value (2nd-sub-exp nexp)))))))
+
+(define 1st-sub-exp
+  (lambda (aexp)
+    (car aexp)))
+
+(define 2nd-sub-exp
+  (lambda (aexp)
+    (car (cdr (cdr aexp)))))
+
+(define operator
+  (lambda (aexp)
+    (car (cdr aexp))))
+
+(define sero?
+  (lambda (n)
+    (null? n)))
+
+(define edd1
+  (lambda (n)
+    (cons '() n)))
+
+(define zub1
+  (lambda (n)
+    (cdr n)))
+
+(define pluz
+  (lambda (n m)
+    (cond
+      ((sero? m) n)
+      (else (edd1 (pluz n (zub1 m)))))))
